ce458428e3fd3d916bcb1d44a1ecf660eb0e2034
Ege Merey <egemerey@gmail.com>
2020-07-07 13:36:07 +0100

    Update index.js

--- myproject/index.js
+++ myproject/index.js
@@ -12,51 +12,51 @@ request.onload = function() {
   
   var data = JSON.parse(this.response)
      
-    data.forEach(asd => {
+    data.forEach(lineName => {
       
-  if(asd.id == "bakerloo"){
-      var a = asd.lineStatuses[0].statusSeverityDescription
-      document.getElementById("bakerlooStatus").textContent = a;
+  if(lineName.id == "bakerloo"){
+      var statusInfo = lineName.lineStatuses[0].statusSeverityDescription
+      document.getElementById("bakerlooStatus").textContent = statusInfo;
   }
-  else if(asd.id == "central"){
-    var a = asd.lineStatuses[0].statusSeverityDescription
-    document.getElementById("centralStatus").textContent = a    
+  else if(lineName.id == "central"){
+    var statusInfo = lineName.lineStatuses[0].statusSeverityDescription
+    document.getElementById("centralStatus").textContent = statusInfo; 
   } 
-  else if(asd.id == "circle"){
-    var a = asd.lineStatuses[0].statusSeverityDescription
-    document.getElementById("circleStatus").textContent= a;
+  else if(lineName.id == "circle"){
+    var statusInfo = lineName.lineStatuses[0].statusSeverityDescription
+    document.getElementById("circleStatus").textContent= statusInfo;
   }
-  else if(asd.id == "district"){
-    var a = asd.lineStatuses[0].statusSeverityDescription
-    document.getElementById("districtStatus").textContent= a;
+  else if(lineName.id == "district"){
+    var statusInfo = lineName.lineStatuses[0].statusSeverityDescription
+    document.getElementById("districtStatus").textContent= statusInfo;
   }
-  else if(asd.id == "hammersmith-city"){
-    var a = asd.lineStatuses[0].statusSeverityDescription
-    document.getElementById("Hammersmith&CityStatus").textContent= a;
+  else if(lineName.id == "hammersmith-city"){
+    var statusInfo = lineName.lineStatuses[0].statusSeverityDescription
+    document.getElementById("Hammersmith&CityStatus").textContent= statusInfo;
   }
-  else if(asd.id == "jubilee"){
-    var a = asd.lineStatuses[0].statusSeverityDescription
-    document.getElementById("jubileeStatus").textContent= a;
+  else if(lineName.id == "jubilee"){
+    var statusInfo = lineName.lineStatuses[0].statusSeverityDescription
+    document.getElementById("jubileeStatus").textContent= statusInfo;
   }
-  else if(asd.id == "metropolitan"){
-    var a = asd.lineStatuses[0].statusSeverityDescription
-    document.getElementById("metropolitanStatus").textContent= a;
+  else if(lineName.id == "metropolitan"){
+    var statusInfo = lineName.lineStatuses[0].statusSeverityDescription
+    document.getElementById("metropolitanStatus").textContent= statusInfo;
   }
-  else if(asd.id == "northern"){
-    var a = asd.lineStatuses[0].statusSeverityDescription
-    document.getElementById("northernStatus").textContent= a;
+  else if(lineName.id == "northern"){
+    var statusInfo = lineName.lineStatuses[0].statusSeverityDescription
+    document.getElementById("northernStatus").textContent= statusInfo;
   }
-  else if(asd.id == "piccadilly"){
-    var a = asd.lineStatuses[0].statusSeverityDescription
-    document.getElementById("piccadillyStatus").textContent= a;
+  else if(lineName.id == "piccadilly"){
+    var statusInfo = lineName.lineStatuses[0].statusSeverityDescription
+    document.getElementById("piccadillyStatus").textContent= statusInfo;
   }
-  else if(asd.id == "victoria"){
-    var a = asd.lineStatuses[0].statusSeverityDescription
-    document.getElementById("victoriaStatus").textContent= a;
+  else if(lineName.id == "victoria"){
+    var statusInfo = lineName.lineStatuses[0].statusSeverityDescription
+    document.getElementById("victoriaStatus").textContent= statusInfo;
   }
-  else if(asd.id == "waterloo-city"){
-    var a = asd.lineStatuses[0].statusSeverityDescription
-    document.getElementById("waterloocityStatus").textContent= a;
+  else if(lineName.id == "waterloo-city"){
+    var statusInfo = lineName.lineStatuses[0].statusSeverityDescription
+    document.getElementById("waterloocityStatus").textContent= statusInfo;
   }
 })
 }
